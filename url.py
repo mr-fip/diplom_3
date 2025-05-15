ac9df0c1fcafbf26b6fb329a5c199cff61cdd3eb
mr-fip <sip.tlife@yandex.ru>
2025-05-15 20:55:47 +0300

    Add files via upload

--- url.py
+++ url.py
@@ -1,12 +1,12 @@
 class Url:
     
     BASE_URL = 'https://stellarburgers.nomoreparties.site'
-    order_history_profile_url = BASE_URL + "/account/order-history"
-    user_data_management_url = BASE_URL + "/api/auth/user"
-    forgot_password_url =  BASE_URL + "/forgot-password"
-    reset_password_url =  BASE_URL + "/reset-password"
-    profile_page_url = BASE_URL + '/account/profile'
-    create_user = BASE_URL + "/api/auth/register"
-    order_feed_page_url = BASE_URL + '/feed'
-    login_page_url = BASE_URL + '/login'
-    main_page_url = BASE_URL + '/'
+    ORDER_HISTORY_PROFILE_URL = BASE_URL + "/account/order-history"
+    USER_DATA_MANAGEMENT_URL = BASE_URL + "/api/auth/user"
+    FORGOT_PASSWORD_URL =  BASE_URL + "/forgot-password"
+    RESET_PASSWORD_URL =  BASE_URL + "/reset-password"
+    PROFILE_PAGE_URL = BASE_URL + '/account/profile'
+    CREATE_USER = BASE_URL + "/api/auth/register"
+    ORDER_FEED_PAGE_URL = BASE_URL + '/feed'
+    LOGIN_PAGE_URL = BASE_URL + '/login'
+    MAIN_PAGE_URL = BASE_URL + '/'
